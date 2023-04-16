9af34234308268f0146e8b0a652bc6ff4833268a
FerQue <fernando_quezada1785@outlook.com>
2023-04-16 16:15:43 -0400

    Update_1
    Update_1

---
+++ CreditCardScreen/CreditCardScreen/CreditCardScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardScreenApp.swift
+//  CreditCardScreen
+//
+//  Created by Victor Quezada on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
